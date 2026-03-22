b297bd87379970908fb2c8bf0f510fb697812d3b
Prajan155 <darkmatrix747@gmail.com>
2026-03-22 19:26:35 +0530

    allow vercel frontend cors

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:8000",
+        "https://finpulse-nexus.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
